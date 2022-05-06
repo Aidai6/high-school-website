efff906f311d69a7ae28d64f3af2f669fa963b13
Aidai Almaz kyzy <aalmazkyzy@gmail.com>
2022-05-06 23:28:47 +0600

    Added @media queries to header c

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons/faXmark';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  // @ViewChild('navLinks'):
+  faXmark = faXmark;
+  faBars = faBars;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // onHideMenu() {
+  //   return this.navLinks.setStyle('right: -200px');
+  //
+  // }
+  //
+  // onShowMenu() {
+  //   return this.navLinks.setStyle('right: 0');
+  //
+  // }
 }
